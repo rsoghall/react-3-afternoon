a8fd463c550a7e4f2e2b0314488cb7211d04a81c
Ryan <ryan@Ryans-MacBook-Pro.local>
2019-01-17 15:56:44 -0700

    social media platform

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-
+import axios from 'axios'
 import './App.css';
 
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+import Post from '../components/Post/Post.js'
 
 class App extends Component {
   constructor() {
@@ -19,31 +20,64 @@ class App extends Component {
   }
   
   componentDidMount() {
+    axios.get('https://practiceapi.devmountain.com/api/posts')
+    .then((results) => {
+      console.log(results.data)
+      this.setState({posts: results.data})
+    })
 
   }
 
-  updatePost() {
+  updatePost(id, text) {
+    axios.put(`https://practiceapi.devmountain.com/api/posts?id=${id}`, {text})
+    .then((results) => {
+      this.setState({post: results.data})
+    })
+    
   
   }
 
-  deletePost() {
+  deletePost(id) {
+    axios.delete(`https://practiceapi.devmountain.com/api/posts?id=${id}`)
+    .then(results => {
+      this.setState({posts: results.data})
+    })
 
   }
 
-  createPost() {
+  createPost(text) {
+    axios.post(`https://practiceapi.devmountain.com/api/posts?id=${text}`)
+    .then(results => {
+      this.setState({posts: results.data})
+    })
 
   }
 
   render() {
     const { posts } = this.state;
-
+//     let postDisplay = posts.map((item) => {
+//       return item.text
+// console.log(item)
+//     })
     return (
       <div className="App__parent">
         <Header />
 
         <section className="App__content">
 
-          <Compose />
+          <Compose  createPostFn={this.createPost}/>
+          
+
+          {
+            posts.map( post => (
+            <Post key={post.id} 
+            text={post.text}
+            date={post.date}
+            updatePostFn={this.updatePost}
+            deletePostFn={this.deletePost}/>
+          ))
+          }
+          
           
         </section>
       </div>
